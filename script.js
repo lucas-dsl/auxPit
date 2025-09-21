10ae1919f5cc7f81676c6faaa91054df7e2267e4
Lucas Silva <silvalucas71743@gmail.com>
2025-09-21 18:59:43 -0300

    ajustando tela de login

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let timer;
 let helpButton = document.getElementById('help-button');
+let chatbotContainer = document.getElementById('chatbot-container');
 let helpShown = false;
 
 function showHelpButton() {
   if (!helpShown) {
     helpButton.style.display = 'block';
-    helpShown = true; // só mostra uma vez
+    helpShown = true; // só aparece uma vez
   }
 }
 
 function resetTimer() {
   if (!helpShown) {
     clearTimeout(timer);
-    timer = setTimeout(showHelpButton, 2000);
+    timer = setTimeout(showHelpButton, 4000);
   }
 }
 
 document.addEventListener('mousemove', resetTimer);
 document.addEventListener('keydown', resetTimer);
+resetTimer();
 
-resetTimer(); // inicializa o timer
+// Alternar chatbot ao clicar no botão
+helpButton.addEventListener('click', () => {
+  if (chatbotContainer.style.display === 'block') {
+    chatbotContainer.style.display = 'none';
+  } else {
+    chatbotContainer.style.display = 'block';
+  }
+});
